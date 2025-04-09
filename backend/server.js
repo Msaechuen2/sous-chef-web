aef1dd69e76b38ad0b072fa1668281e20c1fff36
Msaechuen2 <bank.msaechueng@gmail.com>
2025-04-09 23:35:10 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,8 @@ const PORT = 5001;
 const mongoose = require('mongoose');
 const Recipe = require('./models/Recipes');
 const recipeRoutes = require('./routes/recipe');
-const User = require('./models/User')
-const dotenv = require('doten
+const User = require('./models/User');
+const dotenv = require('dotenv');
 const userRoutes = require("./routes/user");
 const router = express.Router();
 
